4f0cdc1ab7f155ecf1768b4341f91c414432d5c7
Harrison Billings <harry-mac@Harrisons-Laptop.local>
2025-04-23 23:02:18 -0700

    services are back in buisness

--- electron/services/generated-documents.ts
+++ electron/services/generated-documents.ts
@@ -1,47 +1,47 @@
-// import { Result } from "../../shared/utils/result";
-// import { db } from "../db/client";
-// import { pages } from "../db/schema";
-// import { and, eq } from "drizzle-orm";
+import { Result } from "../../shared/utils/result";
+import { db } from "../db/client";
+import { pages } from "../db/schema";
+import { and, eq } from "drizzle-orm";
 
-// export async function getGeneratedPages() {
-//   const res = await Result.fromAsync(() => db.select().from(pages));
+export async function getGeneratedPages() {
+  const res = await Result.fromAsync(() => db.select().from(pages));
 
-//   return res.getOrThrow();
-// }
+  return res.getOrThrow();
+}
 
-// export async function saveDocument(doc: {
-//   content: string;
-//   name: string;
-//   projectId: number;
-// }) {
-//   const res = await Result.fromAsync(() =>
-//     db
-//       .insert(pages)
-//       .values([{ createdAt: new Date(), ...doc }])
-//       .returning(),
-//   );
+export async function saveDocument(doc: {
+  content: string;
+  name: string;
+  projectId: number;
+}) {
+  const res = await Result.fromAsync(() =>
+    db
+      .insert(pages)
+      .values([{ createdAt: new Date(), ...doc }])
+      .returning(),
+  );
 
-//   return res
-//     .map((ele) => ele.at(0))
-//     .ifNull("Could not find the requested record")
-//     .getOrThrow();
-// }
+  return res
+    .map((ele) => ele.at(0))
+    .ifNull("Could not find the requested record")
+    .getOrThrow();
+}
 
-// export async function getGeneratedDoc(query: {
-//   docId: number;
-//   projectId: number;
-// }) {
-//   const res = await Result.fromAsync(() =>
-//     db
-//       .select()
-//       .from(pages)
-//       .where(
-//         and(eq(pages.id, query.docId), eq(pages.projectId, query.projectId)),
-//       ),
-//   );
+export async function getGeneratedDoc(query: {
+  docId: number;
+  projectId: number;
+}) {
+  const res = await Result.fromAsync(() =>
+    db
+      .select()
+      .from(pages)
+      .where(
+        and(eq(pages.id, query.docId), eq(pages.projectId, query.projectId)),
+      ),
+  );
 
-//   return res
-//     .map((ele) => ele.at(0))
-//     .ifNull("Could not get the requested document")
-//     .getOrThrow()!!;
-// }
+  return res
+    .map((ele) => ele.at(0))
+    .ifNull("Could not get the requested document")
+    .getOrThrow()!!;
+}
